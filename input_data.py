7be03d25982f9b8778371bdc0414d5c34c0f5439
Jiajing LIU <jiajiliu@beaker.cs.ucl.ac.uk>
2020-07-17 03:06:43 +0100

    test

---
+++ input_data.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as sp
+from scipy import io
+import cv2 as cv 
+import matplotlib.pyplot as plt
+import pylab
+import os
+import pandas as pd
+import random
+import json
+
+print(os.listdir(CapsNet-Tensorflow/rpc_data))
+
+hahaha
